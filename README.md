c65a480c20bd8440bdf29aa1b4827fb1414f705c
Tadas <tadas@techconsult.lt>
2020-07-22 01:40:25 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # test-bgfx
+=======
+
+## To update all submodules
+```
+git submodule update --remote --merge
+```
+
+## To get all submodules
+```
+git submodule update --init --recursive
+```
---
+++ examples/test1/main.cpp
@@ -0,0 +1,26 @@
+#include "bigg.hpp"
+#include "entt/entt.hpp"
+
+class ExampleImguiDemo : public bigg::Application
+{
+    void onReset()
+    {
+        bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xc0c0c0ff, 1.0f, 0);
+        bgfx::setViewRect(0, 0, 0, uint16_t(getWidth()), uint16_t(getHeight()));
+    }
+
+    void update(float dt)
+    {
+        bgfx::touch(0);
+        // ImGui::ShowDemoWindow();
+    }
+
+public:
+    ExampleImguiDemo() : bigg::Application("Test 1") {}
+};
+
+int main(int argc, char **argv)
+{
+    ExampleImguiDemo app;
+    return app.run(argc, argv);
+}
